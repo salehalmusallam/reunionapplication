1380322aa13486322c5ba320e24c2b2b4a39ed66
Saleh Almusalam <salehalmusallam@gmail.com>
2015-10-17 14:20:06 +0300

    First commit

---
+++ reuniontwilio.php
@@ -0,0 +1,8 @@
+<?php
+    header('Content-Type: text/xml');
+    $codeWithSpaces = implode(', ',str_split($_GET["code"]));
+    echo '<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+<Say voice="alice" loop="2">Your confirmation code is '.$codeWithSpaces.'</Say>
+</Response>';
+?>
